8846e1c416e470ccfe9a535d9b00981b1334a5b2
Hugo Zul <hugozul@google.com>
2020-12-08 18:26:06 -0800

    comment out unused block

--- src/main/java/com/woodencloset/signalbot/responders/DiceRollResponder.java
+++ src/main/java/com/woodencloset/signalbot/responders/DiceRollResponder.java
@@ -17,19 +17,19 @@ public class DiceRollResponder implements SignalBot.Responder {
     @Override
     public String getResponse(String messageText) {
         return "FOO BAR!";
-        Matcher matcher = DICE_ROLL_PATTERN.matcher(messageText);
-        if (matcher.matches()) {
-            int numDice = Math.min(Integer.parseInt(matcher.group("dice")), MAX_DICE);
-            int numSides = Math.min(Integer.parseInt(matcher.group("sides")), MAX_SIDES);
-            String flag = matcher.group("flag");
-            boolean sorted = flag != null && flag.equalsIgnoreCase("s");
-            IntStream randoms = RANDOM.ints(numDice, 1, numSides + 1);
-            if (sorted) {
-                randoms = randoms.sorted();
-            }
-            String randomString = randoms.mapToObj(String::valueOf).collect(Collectors.joining(", ", "[", "]"));
-            return String.format("%dd%d: %s", numDice, numSides, randomString);
-        }
-        return null;
+        // Matcher matcher = DICE_ROLL_PATTERN.matcher(messageText);
+        // if (matcher.matches()) {
+        //     int numDice = Math.min(Integer.parseInt(matcher.group("dice")), MAX_DICE);
+        //     int numSides = Math.min(Integer.parseInt(matcher.group("sides")), MAX_SIDES);
+        //     String flag = matcher.group("flag");
+        //     boolean sorted = flag != null && flag.equalsIgnoreCase("s");
+        //     IntStream randoms = RANDOM.ints(numDice, 1, numSides + 1);
+        //     if (sorted) {
+        //         randoms = randoms.sorted();
+        //     }
+        //     String randomString = randoms.mapToObj(String::valueOf).collect(Collectors.joining(", ", "[", "]"));
+        //     return String.format("%dd%d: %s", numDice, numSides, randomString);
+        // }
+        // return null;
     }
 }
